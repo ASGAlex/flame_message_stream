cde5ba145e8811b0a07b7a107ca5da9fb31aed26
Alexey Volkov <asgalex@gmail.com>
2023-02-06 19:30:29 +0300

    docs: prepare for publishing

--- lib/flame_message_stream.dart
+++ lib/flame_message_stream.dart
@@ -1,8 +1,8 @@
 /// Simple base classes wor easy management of streams in Flame
-/// Use [MessageStreamProvider] to add ability to send messages for any class of
+/// Use "MessageStreamProvider" to add ability to send messages for any class of
 /// application.
-/// Use [MessageListener] mixin to listen messages in Flame's components
-/// Use [HasMessageProviders] mixin for Game class to have ability to send
+/// Use "MessageListener" mixin to listen messages in Flame's components
+/// Use "HasMessageProviders" mixin for Game class to have ability to send
 /// messages from the game
 ///
 /// Streams could be used to communicate between components, between game and
